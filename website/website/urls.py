f9082db1db6c3526fff80d54bde411ae27a7d846
jihadj0002 <87649538+jihadj0002@users.noreply.github.com>
2024-05-25 19:38:44 +0600

    new

--- website/website/urls.py
+++ website/website/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("task.urls"))
+    path("", include("task.urls")),
+    path("", include("user.urls")),
+    
 ]
