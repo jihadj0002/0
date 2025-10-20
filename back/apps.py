b44f1df3ce9fd936cfb47f85cae89bf4500f157f
Jubayer Ahamed <jihadj0002@gmail.com>
2025-10-20 10:36:04 +0600

    Starting MongoDB on apps in Back

--- back/apps.py
+++ back/apps.py
@@ -1,6 +1,25 @@
 from django.apps import AppConfig
 
 
+from mongoengine import connect
+import os
+
+
 class BackConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "back"
+
+
+# Setup MongoDB
+
+
+
+
+class MyAppConfig(AppConfig):
+    name = "mongo"
+
+    def ready(self):
+        # Read URI from env var or Django settings
+        
+        mongo_uri = os.environ.get("mongodb+srv://jihadj0002_db_user:p9oaBw3ClpZF69m4@cluster0.fxkjzog.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        connect(host=mongo_uri, alias="default")   # alias "default" for simplicity
