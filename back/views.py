66f92c85067dc7e663fd285ede483b8781411ced
jihadj0002 <87649538+jihadj0002@users.noreply.github.com>
2025-10-22 11:16:40 +0000

    Added login required at back pages

--- back/views.py
+++ back/views.py
@@ -2,22 +2,25 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+
+@login_required
 def dashboard(request):
+    print(request.user)
     return render(request, "back/dashboard.html", {"user": request.user})
 
-
-
-
-login_required()
+@login_required
 def c_dashboard(request):
     return render(request, "back/c_dashboard.html", {"user": request.user})
 
+@login_required
 def products(request):
     return render(request, "back/products.html", {"user": request.user})
 
+@login_required
 def stats(request):
     return render(request, "back/stats.html", {"user": request.user})
 
+@login_required
 def sett(request):
     return render(request, "back/options.html", {"user": request.user})
 
