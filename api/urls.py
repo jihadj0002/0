46db0f7dbc499b532d16c81d027a86c51f75c6d5
Jubayer Ahamed <jihadj0002@gmail.com>
2025-10-29 02:32:49 +0600

    Updated API

--- api/urls.py
+++ api/urls.py
@@ -13,7 +13,8 @@ urlpatterns = [
     path('', include(router.urls)),
 
     path('<str:username>/product_list', views.UserProductListView.as_view(), name='user-product-list'),
-    path('<str:username>/product/<int:pk>', views.UserProductDataView.as_view(), name='user-product-update'),
+    path('<str:username>/product/<int:pk>/', views.UserProductDataView.as_view(), name='user-product-view'),
+
     path('<str:username>/product/<int:pk>/update', views.UserProductUpdateView.as_view(), name='user-product-update'),
     path('<str:username>/orders', views.UserOrderListCreateView.as_view(), name='user-order-list'),
 
