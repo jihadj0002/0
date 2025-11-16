0a4c9d3e8b13849a04b48c92caa4075881044e43
Jubayer Ahamed <jihadj0002@gmail.com>
2025-11-16 21:08:40 +0600

    Updated msg

--- msg/views.py
+++ msg/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def message_list(request):
-    return HttpResponse("✅ Thank you! We’ll contact you soon.")
+    return HttpResponse("✅ Thank you! We’ll contact you soon.Message Section is working.")
     # return render(request, "messages/message_list.html", {})
