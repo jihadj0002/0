256bdcd4d7e49eebebadd9e80fdc41e3a9ba800d
Jubayer Ahamed <jihadj0002@gmail.com>
2025-12-05 20:15:23 +0600

    Hidden MSG Button

--- api/urls.py
+++ api/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
 
     path('<str:username>/product_list', views.UserProductListView.as_view(), name='user-product-list'),
     path('<str:username>/product/<int:pk>/', views.UserProductDataView.as_view(), name='user-product-view'),
-
     path('<str:username>/product/<int:pk>/update', views.UserProductUpdateView.as_view(), name='user-product-update'),
+
     path('<str:username>/orders', views.UserOrderListCreateView.as_view(), name='user-order-list'),
     path('<str:username>/orders/add', views.UserOrderCreateView.as_view(), name='user-order-add'),
 
