ca5d996a940ab7e15873cad238db08bc42c236fb
Jubayer Ahamed <jihadj0002@gmail.com>
2026-02-23 17:44:49 -0500

    Updated selected_product length in model

--- back/models.py
+++ back/models.py
@@ -261,7 +261,7 @@ class Conversation(models.Model):
 
     last_viewed_product = models.CharField(max_length=255, blank=True, null=True)
     detected_intent = models.CharField(max_length=100, blank=True, null=True)
-    current_product = models.CharField(max_length=1500, blank=True, null=True)
+    current_product = models.CharField(max_length=2500, blank=True, null=True)
     current_package = models.CharField(max_length=1500, blank=True, null=True)
     extra_data = models.JSONField(blank=True, null=True)
 
